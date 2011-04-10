d2a007b771c982dba96ab9f1b4a1a8961a3a8905
Evijair Nuñez <nujev2008@gmail.com>
2011-04-10 16:39:58 -0500

    algo en bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -33,7 +33,7 @@ class BootStrap {
 
 		log.debug "Creando usuario Admin"
         def usuariot = Usuario.findByUsername('test')
-        if (!usuario){
+        if (!usuariot){
             usuariot = new Usuario(
                 username : 'test'
                 , password : springSecurityService.encodePassword('test')
